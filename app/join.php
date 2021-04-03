58145d1df6623c47a7c0cd594198c1028b25982f
Nicolas Went <nicolas.went@epita.fr>
2021-04-03 15:49:15 +0200

    [Join] Put the copied and pasted join to fix the problem

--- app/join.php
+++ app/join.php
@@ -36,9 +36,22 @@ if($request['method'] == "POST")
                         'time' => getTimeFloat()
                     ]);
 
+                    /*
+                     * old
                     header("HTTP/1.1 204 NO CONTENT");
                     // header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-                    // remove the above lign otherwise minecraft does not recognise the result
+                    // remove the above lign otherwise minecraft does not recognise the result*/
+
+                    // copied and pasted 204 from: https://gist.github.com/msdousti/b98f225512bd21ba23caebc5548f4678
+                    ob_start();
+
+                    header("HTTP/1.1 204 NO CONTENT");
+
+                    header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+                    header("Pragma: no-cache"); // HTTP 1.0.
+                    header("Expires: 0"); // Proxies.
+
+                    ob_end_flush(); //now the headers are sent
 
                 } else {
                     header('Content-Type: application/json');
